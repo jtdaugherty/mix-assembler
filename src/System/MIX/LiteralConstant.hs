79cdd62d30fd0af078b5d443739b75b1e5ec2332
Jonathan Daugherty <cygnus@foobox.com>
2012-12-02 19:05:27 -0800

    Explain litconst implementation

--- src/System/MIX/LiteralConstant.hs
+++ src/System/MIX/LiteralConstant.hs
@@ -5,6 +5,31 @@ where
 
 import System.MIX.Symbolic
 
+-- Rewrite a MIXAL program so that literal constants are moved into
+-- memory cells, with the instructions that referenced them updated to
+-- use PC-relative addresses.  This works by inserting the constant
+-- instruction just prior to the place it is referenced, and also
+-- inserting a jump just prior to *that* to ensure that program flow
+-- jumps over the constant, like so:
+--
+--       LDA =...=
+--
+--  becomes
+--
+--       JMP *+2
+--       CON ...
+--       LDA *-1
+--
+-- This is not how Knuth describes it and it has the (potentially
+-- serious) downside that the MIX programmer cannot reason about the
+-- size of a program segment (i.e. the sequence of instructions
+-- between ORIG directives).  Ultimately, this needs to be fixed so
+-- that we actually support "forward references" in the style
+-- described in TAOCP; that is going to require the assembler routine
+-- to be rewritten to support this, though, since it means we have to
+-- evaluate symbols that haven't been defined at the time they are
+-- encountered.
+
 rewriteLiteralConstants :: [MIXALStmt] -> [MIXALStmt]
 rewriteLiteralConstants = concatMap rewrite
 
