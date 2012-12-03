12571dff2940b332410580597f82124e59f0b4eb
Jonathan Daugherty <cygnus@foobox.com>
2012-12-02 19:37:46 -0800

    Main cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,10 @@ module Main where
 
 import System.Environment (getArgs)
 import Text.PrettyPrint.HughesPJ
-import Control.Applicative
 
 import System.MIX.MIXALParser
 import System.MIX.PP
 import System.MIX.Assembler
-import System.MIX.LiteralConstant
 
 formatMessage :: AsmError -> String
 formatMessage (AsmError s Nothing) = s
@@ -27,25 +25,12 @@ main = do
   let r = parseMIXAL fname s
   case r of
     Left e -> putStrLn $ "Error: " ++ show e
-    Right is -> do
-           mapM_ (putStrLn . show) is
-           putStrLn ""
-           putStrLn $ render $ vcat $ ppMIXALStmt <$> is
-
-           putStrLn "Rewritten results:"
-           mapM_ (putStrLn . show) $ rewriteLiteralConstants is
-
-           putStrLn "Rewritten results (pretty):"
-           putStrLn ""
-           putStrLn $ render $ vcat $ ppMIXALStmt <$> rewriteLiteralConstants is
-
-           let result = assemble is
-
-           case result of
-             Left e -> do
-                  putStrLn "Error during assembly:"
-                  putStrLn $ formatMessage e
-             Right res -> do
-                  putStrLn "Assembler output:"
-                  putStrLn ""
-                  putStrLn $ render $ ppProgram $ program res
+    Right is ->
+        case assemble is of
+          Left e -> do
+            putStrLn "Error during assembly:"
+            putStrLn $ formatMessage e
+          Right res -> do
+            putStrLn "Assembler output:"
+            putStrLn ""
+            putStrLn $ render $ ppProgram $ program res
